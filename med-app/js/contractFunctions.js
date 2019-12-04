671a71532feebe92f322e8e43fe4af06eb2eaa50
Yuri <yuri.strack@gmail.com>
2019-12-04 12:19:53 -0300

    Creating tests for Sales registers

--- med-app/js/contractFunctions.js
+++ med-app/js/contractFunctions.js
@@ -37,8 +37,6 @@ module.exports = {
         const contract = network.getContract('medication-contract');
 
         // Submit the specified transaction.
-        // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
-        // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
         await contract.submitTransaction('createMedication', medicationId, manufactorId, medicationName, medicationFabricationDate, medicationExpDate);
         console.log('Transaction has been submitted');
 
@@ -77,8 +75,6 @@ module.exports = {
             const contract = network.getContract('medication-contract');
     
             // Submit the specified transaction.
-            // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
-            // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
             console.log("Sumbmiting createPrescription Transaction");
             await contract.submitTransaction('createPrescription', prescriptionId, medications, patientId, doctorId);
             console.log('Transaction has been submitted');
@@ -92,7 +88,7 @@ module.exports = {
         }
 
     },
-    sellMedication: async(medicationId, prescriptionId) => {
+    sellMedication: async(saleId, medicationId, prescriptionId) => {
         try {
 
             // Create a new file system based wallet for managing identities.
@@ -119,18 +115,132 @@ module.exports = {
             const contract = network.getContract('medication-contract');
     
             // Submit the specified transaction.
-            // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
-            // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
             const resultPrescription = await contract.submitTransaction('verifyPrescription', medicationId, prescriptionId);
             
-            const resultMedication = await contract.submitTransaction('updateMedication', medicationId, prescriptionId);
+            await contract.submitTransaction('updateMedication', medicationId, prescriptionId);
+            await contract.submitTransaction('createRegister', saleId, medicationId, prescriptionId);
             console.log('Transaction has been submitted');
-    
+            
             // Disconnect from the gateway.
             await gateway.disconnect();
-    
+
+            return {status: "ok", message: ""};
         } catch (error) {
             console.error(`Failed to submit transaction: ${error}`);
+            return {status: "Fail", message: ""};
+        }
+    },
+    queryAllMedication: async() =>{
+        try {
+
+            // Create a new file system based wallet for managing identities.
+            const walletPath = path.join(process.cwd(), 'wallet');
+            const wallet = new FileSystemWallet(walletPath);
+            console.log(`Wallet path: ${walletPath}`);
+    
+            // Check to see if we've already enrolled the user.
+            const userExists = await wallet.exists('user1');
+            if (!userExists) {
+                console.log('An identity for the user "user1" does not exist in the wallet');
+                console.log('Run the registerUser.js application before retrying');
+                return;
+            }
+    
+            // Create a new gateway for connecting to our peer node.
+            const gateway = new Gateway();
+            await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true } });
+    
+            // Get the network (channel) our contract is deployed to.
+            const network = await gateway.getNetwork('mychannel');
+    
+            // Get the contract from the network.
+            const contract = network.getContract('medication-contract');
+    
+            // Evaluate the specified transaction.
+            const resultMedication = await contract.evaluateTransaction('queryAllMedication');
+            console.log(`Medications: ${resultMedication.toString()}`);
+    
+            console.log("\n");
+            return resultMedication;
+    
+        } catch (error) {
+            console.error(`Failed to evaluate transaction: ${error}`);
+            process.exit(1);
+        }
+    },
+    queryAllPrescriptions: async() => {
+        try {
+
+            // Create a new file system based wallet for managing identities.
+            const walletPath = path.join(process.cwd(), 'wallet');
+            const wallet = new FileSystemWallet(walletPath);
+            console.log(`Wallet path: ${walletPath}`);
+    
+            // Check to see if we've already enrolled the user.
+            const userExists = await wallet.exists('user1');
+            if (!userExists) {
+                console.log('An identity for the user "user1" does not exist in the wallet');
+                console.log('Run the registerUser.js application before retrying');
+                return;
+            }
+    
+            // Create a new gateway for connecting to our peer node.
+            const gateway = new Gateway();
+            await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true } });
+    
+            // Get the network (channel) our contract is deployed to.
+            const network = await gateway.getNetwork('mychannel');
+    
+            // Get the contract from the network.
+            const contract = network.getContract('medication-contract');
+
+            // Evaluate the specified transaction.
+            const resultPrescription =  await contract.evaluateTransaction('queryAllPrescription');
+            console.log(`Prescriptions: ${resultPrescription.toString()}`);
+    
+            console.log("\n");
+            return resultPrescription;
+    
+        } catch (error) {
+            console.error(`Failed to evaluate transaction: ${error}`);
+            process.exit(1);
+        }
+    },
+    queryAllSales: async() => {
+        try {
+
+            // Create a new file system based wallet for managing identities.
+            const walletPath = path.join(process.cwd(), 'wallet');
+            const wallet = new FileSystemWallet(walletPath);
+            console.log(`Wallet path: ${walletPath}`);
+    
+            // Check to see if we've already enrolled the user.
+            const userExists = await wallet.exists('user1');
+            if (!userExists) {
+                console.log('An identity for the user "user1" does not exist in the wallet');
+                console.log('Run the registerUser.js application before retrying');
+                return;
+            }
+    
+            // Create a new gateway for connecting to our peer node.
+            const gateway = new Gateway();
+            await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true } });
+    
+            // Get the network (channel) our contract is deployed to.
+            const network = await gateway.getNetwork('mychannel');
+    
+            // Get the contract from the network.
+            const contract = network.getContract('medication-contract');
+            
+            // Evaluate the specified transaction.
+            const resultSales =  await contract.evaluateTransaction('queryAllSales');
+            console.log(`Sales: ${resultSales.toString()}`);
+    
+            console.log("\n");
+            return resultSales;
+    
+        } catch (error) {
+            console.error(`Failed to evaluate transaction: ${error}`);
             process.exit(1);
         }
     }
--- med-app/js/test.js
+++ med-app/js/test.js
@@ -10,8 +10,17 @@ async function main() {
     await functions.createPrescription('PES10', '{"payload":[{"name":"MED0"}]}', "11111111111", "4444");
     await functions.createPrescription('PES11', '{"payload":[{"name":"MED1"},{"name":"MED11"}]}', "11111111111", "4444");
 
+    console.log("Querying All Medications:");
+    await functions.queryAllMedication();
+
+    console.log("Querying All Prescriptions:");
+    await functions.queryAllPrescriptions();
+
     console.log("Selling medicine");
-    await functions.sellMedication("MED0", "PES10");
+    await functions.sellMedication("SALE1", "MED0", "PES10");
+
+    console.log("Querying All Sales:");
+    await functions.queryAllSales();
 }
 
 main();
