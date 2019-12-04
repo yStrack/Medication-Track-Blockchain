9df0414b1a3cc6da8bdea9867f61ccbb7a8a5cf4
Yuri <yuri.strack@gmail.com>
2019-12-04 19:25:12 -0300

    Adding new routes for creating medication,prescription and sell med

--- med-app/js/controller.js
+++ med-app/js/controller.js
@@ -12,5 +12,64 @@ module.exports = {
   async queryAllSales(req, res) {
     const response = await functions.queryAllSales();
     return res.send(response.toString());
+  },
+  async createMedication(req, res) {
+    const { medicationId } = req.body;
+    const { manufactorId } = req.body;
+    const { medicationName } = req.body;
+    const { fabDate } = req.body;
+    const { expDate } = req.body;
+
+    try {
+      await functions.createMedication(
+        medicationId,
+        manufactorId,
+        medicationName,
+        fabDate,
+        expDate
+      );
+      return res.send({ status: "ok", message: "" });
+    } catch (err) {
+      return res.send({
+        status: "Fail",
+        message: `Failed to create new medicine ${err}`
+      });
+    }
+  },
+  async createPrescription(req, res) {
+    const { prescriptionId } = req.body;
+    var { medications } = req.body;
+    const { patientId } = req.body;
+    const { doctorId } = req.body;
+
+    try {
+      await functions.createPrescription(
+        prescriptionId,
+        JSON.stringify(medications),
+        patientId,
+        doctorId
+      );
+      return res.send({ status: "ok", message: "" });
+    } catch (err) {
+      return res.send({
+        status: "Fail",
+        message: `Failed to create new prescription ${err}`
+      });
+    }
+  },
+  async sellMedication(req, res) {
+    const { saleId } = req.body;
+    const { prescriptionId } = req.body;
+    const { medicationId } = req.body;
+
+    try {
+      await functions.sellMedication(saleId, medicationId, prescriptionId);
+      return res.send({ status: "ok", message: "" });
+    } catch (err) {
+      return res.send({
+        status: "Fail",
+        message: `Failed to create new prescription ${err}`
+      });
+    }
   }
 };
--- med-app/js/routes.js
+++ med-app/js/routes.js
@@ -5,5 +5,8 @@ const routes = express.Router();
 routes.get("/get_all_meds", contract.queryAllMedication);
 routes.get("/get_all_presc", contract.queryAllPrescription);
 routes.get("/get_all_sells", contract.queryAllSales);
+routes.post("/create_med", contract.createMedication);
+routes.post("/create_pres", contract.createPrescription);
+routes.post("/sell_med", contract.sellMedication);
 
 module.exports = routes;
