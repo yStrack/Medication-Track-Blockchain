951145a1f40c5f19f77368e26d8522af402fc1d0
Yuri Zoel Brasil <yuri.zoel@olxbr.com>
2019-12-05 16:05:47 -0300

    Doctor dashboard, submit button and other fields

--- frontend/src/pages/doctor/index.js
+++ frontend/src/pages/doctor/index.js
@@ -5,7 +5,7 @@ export default function DoctorDashboard() {
     const [data,setData] = React.useState({
         medications: [{
             name:"",
-            quantity:"",
+            dosage:"",
         }],
         patientId: "",
         doctorId: ""
@@ -19,13 +19,17 @@ export default function DoctorDashboard() {
       console.log("data:",data)
       setData({...data, medications: data.medications.concat({
           name:"",
-          quantity:""
+          dosage:""
         })
       })
     }
 
+    const handleSubmit = () => {
+      //#TODO
+    }
+
     return (
-      <Card style={{position:"absolute", width:"80%", top:"2rem", left:"50%"}}>
+      <Card style={{position:"absolute", width:"80%", top:"2rem", left:"50%", paddingBottom:"4rem"}}>
         <div className="doctor">
           <div className="doctor__information">
               <TextField style={{width:"40%"}} onChange={(event) => setData({...data, patientId:event.target.value})} label="Patient's CPF" />
@@ -40,10 +44,21 @@ export default function DoctorDashboard() {
               }
               return(
               <div key={index} className="doctor__medications">
-                <TextField style={{width:"40%", marginRight: "2rem"}} label="Medication Name" value={data.medications[index].name} onChange={(event) => handleChange("name", event.target.value)} />
-                {/* <TextField label="Quantity in mg" type="number" value={data.medications[index].quantity} onChange={(event) => handleChange("quantity", event.target.value)} /> */}
-                { index == data.medications.length - 1 ?
-                  <a className="doctor__medications__add-button" onClick={() => handleAddMedication()}>&#43;</a>
+                <div>
+                  <TextField style={{width:"40%", marginRight: "4rem"}} label="Medication Name" value={data.medications[index].name} onChange={(event) => handleChange("name", event.target.value)} />
+                  <TextField label="Dosage in mg" type="number" value={data.medications[index].dosage} onChange={(event) => handleChange("dosage", event.target.value)} />
+                </div>                
+                <div className="doctor__medications__description">
+                  <TextField style={{width:"calc(80% + 4rem)", marginRight:"2rem"}} multiline label="Usage description" type="number" value={data.medications[index].dosage} onChange={(event) => handleChange("dosage", event.target.value)} />
+                
+                  { index == data.medications.length - 1 ?
+                    <a className="doctor__medications__add-button" onClick={() => handleAddMedication()}>&#43;</a>
+                    :
+                    <div></div>
+                  }
+                </div>
+                { index != data.medications.length - 1 ?
+                  <div className="doctor__medications__separator"></div>
                   :
                   <div></div>
                 }
@@ -51,6 +66,7 @@ export default function DoctorDashboard() {
             )})
           }
         </div>
+        <a className="doctor__submit-button" onClick={() => handleSubmit()}>Submit</a>
       </Card>
     )
 }
